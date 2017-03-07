d066986959b17752a70fe5f5329251c7837f5615
Diego Zamboni <diego@zzamboni.org>
2017-03-07 08:35:49 +0100

    Added fish_debug function, prints only when __fish_notify_debug is set

--- functions/fish_debug.fish
+++ functions/fish_debug.fish
@@ -1,5 +1,5 @@
 function fish_debug -d "Print a debug message"
         if set -q __fish_notify_debug
-                fish_notify brblack "$argv"
+                fish_notify brblack "DEBUG: $argv"
         end
 end
